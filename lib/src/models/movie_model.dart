745ea64eb2fc0935f58210c3861eea2f7ff62fde
Kelven Galvão <kelvengalvao@gmail.com>
2020-01-15 14:58:37 -0300

    Remove unused functions

--- lib/src/models/movie_model.dart
+++ lib/src/models/movie_model.dart
@@ -1,76 +1,42 @@
 import 'dart:convert';
 
-MovieModel movieAppFromJson(String str) => MovieModel.fromJson(json.decode(str));
-
-String movieAppToJson(MovieModel data) => json.encode(data.toJson());
+MovieModel movieModelFromJson(String str) =>
+    MovieModel.fromJson(json.decode(str));
 
 class MovieModel {
-    double popularity;
-    int voteCount;
-    bool video;
-    String posterPath;
-    int id;
-    bool adult;
-    String backdropPath;
-    String originalLanguage;
-    String originalTitle;
-    List<int> genreIds;
-    String title;
-    double voteAverage;
-    String overview;
-    DateTime releaseDate;
+  double popularity;
+  int voteCount;
+  bool video;
+  String posterPath;
+  int id;
+  bool adult;
+  String backdropPath;
+  String originalLanguage;
+  String originalTitle;
+  List<int> genreIds;
+  String title;
+  double voteAverage;
+  String overview;
+  DateTime releaseDate;
 
-    MovieModel({
-        this.popularity,
-        this.voteCount,
-        this.video,
-        this.posterPath,
-        this.id,
-        this.adult,
-        this.backdropPath,
-        this.originalLanguage,
-        this.originalTitle,
-        this.genreIds,
-        this.title,
-        this.voteAverage,
-        this.overview,
-        this.releaseDate,
-    });
+  MovieModel({
+    this.popularity,
+    this.voteCount,
+    this.video,
+    this.posterPath,
+    this.id,
+    this.adult,
+    this.backdropPath,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.title,
+    this.voteAverage,
+    this.overview,
+    this.releaseDate,
+  });
 
-    MovieModel copyWith({
-        double popularity,
-        int voteCount,
-        bool video,
-        String posterPath,
-        int id,
-        bool adult,
-        String backdropPath,
-        String originalLanguage,
-        String originalTitle,
-        List<int> genreIds,
-        String title,
-        double voteAverage,
-        String overview,
-        DateTime releaseDate,
-    }) => 
-        MovieModel(
-            popularity: popularity ?? this.popularity,
-            voteCount: voteCount ?? this.voteCount,
-            video: video ?? this.video,
-            posterPath: posterPath ?? this.posterPath,
-            id: id ?? this.id,
-            adult: adult ?? this.adult,
-            backdropPath: backdropPath ?? this.backdropPath,
-            originalLanguage: originalLanguage ?? this.originalLanguage,
-            originalTitle: originalTitle ?? this.originalTitle,
-            genreIds: genreIds ?? this.genreIds,
-            title: title ?? this.title,
-            voteAverage: voteAverage ?? this.voteAverage,
-            overview: overview ?? this.overview,
-            releaseDate: releaseDate ?? this.releaseDate,
-        );
-
-    factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
+  factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
         popularity: json["popularity"].toDouble(),
         voteCount: json["vote_count"],
         video: json["video"],
@@ -85,22 +51,5 @@ class MovieModel {
         voteAverage: json["vote_average"].toDouble(),
         overview: json["overview"],
         releaseDate: DateTime.parse(json["release_date"]),
-    );
-
-    Map<String, dynamic> toJson() => {
-        "popularity": popularity,
-        "vote_count": voteCount,
-        "video": video,
-        "poster_path": posterPath,
-        "id": id,
-        "adult": adult,
-        "backdrop_path": backdropPath,
-        "original_language": originalLanguage,
-        "original_title": originalTitle,
-        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
-        "title": title,
-        "vote_average": voteAverage,
-        "overview": overview,
-        "release_date": "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
-    };
+      );
 }
