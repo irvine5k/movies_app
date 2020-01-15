e9b706bb1da06e29aa0f3e137bebd94d45185db5
Kelven Galvão <kelvengalvao@gmail.com>
2020-01-15 15:15:59 -0300

    Remove unsed atributes and add tests

--- lib/src/models/movie_model.dart
+++ lib/src/models/movie_model.dart
@@ -4,51 +4,30 @@ MovieModel movieModelFromJson(String str) =>
     MovieModel.fromJson(json.decode(str));
 
 class MovieModel {
-  double popularity;
-  int voteCount;
-  bool video;
   String posterPath;
-  int id;
-  bool adult;
+
   String backdropPath;
-  String originalLanguage;
-  String originalTitle;
+
   List<int> genreIds;
   String title;
-  double voteAverage;
+
   String overview;
   DateTime releaseDate;
 
   MovieModel({
-    this.popularity,
-    this.voteCount,
-    this.video,
     this.posterPath,
-    this.id,
-    this.adult,
     this.backdropPath,
-    this.originalLanguage,
-    this.originalTitle,
     this.genreIds,
     this.title,
-    this.voteAverage,
     this.overview,
     this.releaseDate,
   });
 
   factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
-        popularity: json["popularity"].toDouble(),
-        voteCount: json["vote_count"],
-        video: json["video"],
         posterPath: json["poster_path"],
-        id: json["id"],
-        adult: json["adult"],
         backdropPath: json["backdrop_path"],
-        originalLanguage: json["original_language"],
-        originalTitle: json["original_title"],
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
         title: json["title"],
-        voteAverage: json["vote_average"].toDouble(),
         overview: json["overview"],
         releaseDate: DateTime.parse(json["release_date"]),
       );
---
+++ test/src/models/movie_model_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movies_app/src/models/movie_model.dart';
+
+void main() {
+  const _json = {
+    "poster_path": "/jyw8VKYEiM1UDzPB7NsisUgBeJ8.jpg",
+    "genre_ids": [28, 12, 35, 14],
+    "title": "Jumanji: The Next Level",
+    "overview":
+        "As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored in order to escape the world’s most dangerous game.",
+    "release_date": "2019-12-04"
+  };
+
+  final _posterPath = "/jyw8VKYEiM1UDzPB7NsisUgBeJ8.jpg";
+  final _originalTitle = "Jumanji: The Next Level";
+  final _genreIds = [28, 12, 35, 14];
+  final _title = "Jumanji: The Next Level";
+  final _overview =
+      "As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored in order to escape the world’s most dangerous game.";
+  final _releaseDate = DateTime.parse("2019-12-04");
+
+  test(
+      'When construct a model '
+      'Should present the correct atributes', () {
+    final model = MovieModel(
+      posterPath: _posterPath,
+      genreIds: _genreIds,
+      title: _title,
+      overview: _overview,
+      releaseDate: _releaseDate,
+    );
+
+    expect(model.genreIds, _genreIds);
+    expect(model.title, _title);
+    expect(model.releaseDate, _releaseDate);
+    expect(model.posterPath, _posterPath);
+    expect(model.overview, _overview);
+  });
+
+  test(
+      'When construct a model from a JSON '
+      'Should present the correct atributes', () {
+    final model = MovieModel.fromJson(_json);
+
+    expect(model.genreIds, _genreIds);
+    expect(model.title, _title);
+    expect(model.releaseDate, _releaseDate);
+    expect(model.posterPath, _posterPath);
+    expect(model.overview, _overview);
+  });
+}
